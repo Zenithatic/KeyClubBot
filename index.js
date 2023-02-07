98bd3bf952d5c2f1cc07b440750a853a7399821b
Zenithatic <patmc55555@gmail.com>
2023-02-07 17:08:27 -0500

    Added first functionality
    will react to messages including the words "keyclubbot" or "key club bot"

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
+// Import modules/packages
 const Discord = require('discord.js')
 const tokenModule = require('./token.js')
 
+// Initiate client
 const client = new Discord.Client({
-    intents: 32727
+    intents: [ 
+        Discord.IntentsBitField.Flags.Guilds,
+        Discord.IntentsBitField.Flags.GuildMembers,
+        Discord.IntentsBitField.Flags.GuildMessages,
+        Discord.IntentsBitField.Flags.MessageContent,
+        Discord.IntentsBitField.Flags.GuildMessageReactions,
+        Discord.IntentsBitField.Flags.GuildModeration
+    ]
 })
 
-client.on("ready", (bot) => {
+// Ready callback function
+client.once("ready", (bot) => {
     console.log(bot.user.tag + " is ready")
 })
 
+// On message create
+client.on("messageCreate", message => {
+
+    // Check if message was sent by a bot
+    if (message.author.bot){
+        return
+    }
+
+    let msgContent = message.content.toLowerCase()
+
+    // First test case
+    if (msgContent.includes("keyclubbot") || msgContent.includes("key club bot")){
+        message.react('😄')
+    }
+})
+
+// Log into client
 client.login(tokenModule.token)
