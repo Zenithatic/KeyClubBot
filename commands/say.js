2ef8a0703d132e9d9a3b8a7e34973b718436ce28
Zenithatic <patmc55555@gmail.com>
2023-03-08 20:42:16 -0500

    Make say.js image not required

--- commands/say.js
+++ commands/say.js
@@ -18,16 +18,20 @@ const data = {
         .addAttachmentOption(option => 
             option.setName('image')
             .setDescription('image to upload')
-            .setRequired(true)
+            .setRequired(false)
         ),
     async execute(interaction){
         const channel = interaction.options.getChannel('channel')
         const text = interaction.options.getString('text')
         const image = interaction.options.getAttachment('image')
-        const attch = new Discord.AttachmentBuilder(image.proxyURL)
 
-
-        await channel.send({content: text, files: [attch]})
+        if (image == null){
+            await channel.send({content: text})
+        }
+        else{
+            const attch = new Discord.AttachmentBuilder(image.proxyURL)
+            await channel.send({content: text, files: [attch]})
+        }
 
         await interaction.reply({content: "Message done.", ephemeral: true})
         await setTimeout(() => interaction.deleteReply(), 5000)
